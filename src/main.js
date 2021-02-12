2609d78561060b256a9cc6d85dde5b22c2b536c8
Inyilis Punya <inyilispunya@gmail.com>
2021-02-12 08:11:43 +0700

    up main

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ const users = require('./Routes/users');
 const auth = require('./Routes/auth');
 const { configCloud } = require('./Configs/cloudinary');
 
-routes.use('/inyil/api/product', configCloud, product);
-routes.use('/inyil/api/category', category);
-routes.use('/inyil/api/search', search);
-routes.use('/inyil/api/history', history);
-routes.use('/inyil/api/users', users);
-routes.use('/inyil/api/auth', auth);
+routes.use('/api/product', configCloud, product);
+routes.use('/api/category', category);
+routes.use('/api/search', search);
+routes.use('/api/history', history);
+routes.use('/api/users', users);
+routes.use('/api/auth', auth);
 routes.use('*', (req, res) => respon(res, 209, { msg: 'Not Found' }));
 
 module.exports = routes;
